f82f18e124a22c56cf7db137d9098a5314e1654b
Mohammed Amaan <mohammedamaan9966@gmail.com>
2024-12-03 14:58:52 +0530

    updatd the js file about comment

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ arr.forEach(button => {
          string = "";  // Clear input
          input.value = string;
       } 
+      
       else if (e.target.innerHTML == 'DEL') {
          string = string.substring(0, string.length - 1);  
          input.value = string;
